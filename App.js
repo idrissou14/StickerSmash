d565f7ac8dc0b6ec0fe192caa3d128f2ac33bd23
idrissou14 <131742935+idrissou14@users.noreply.github.com>
2024-09-14 00:18:49 +0100

    add background-image

--- App.js
+++ App.js
@@ -1,10 +1,15 @@
 import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import { StyleSheet, Text, View, Image } from 'react-native';
+
+const PlaceholderImg = require('./assets/images/background-image.png');
 
 export default function App() {
   return (
     <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
+      {/* <Text style={{ color: '#fff' }}>Open up App.js to start working on your app!</Text> */}
+      <View style={styles.imageContainer}>
+        <Image source={PlaceholderImg} style={styles.image} />
+      </View>
       <StatusBar style="auto" />
     </View>
   );
@@ -13,8 +18,17 @@ export default function App() {
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: '#333',
+    backgroundColor: '#25292e',
     alignItems: 'center',
     justifyContent: 'center',
+  },
+  imageContainer: {
+    flex:1,
+    paddingTop:58,
+  },
+  image: {
+    width:320,
+    height:440,
+    borderRadius: 18,
   },
 });
